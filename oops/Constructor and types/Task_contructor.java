94fc97d1d2261dfbfac7bc5a99a51c9c69fa872a
Pugazhendhi M <pugazhendhimtvn@gmail.com>
2025-07-22 16:15:31 +0530

    Add files via upload

---
+++ oops/Constructor and types/Task_contructor.java
@@ -0,0 +1,10 @@
+class Task_contructor{
+     Task_contructor()
+       {
+        System.out.println("Show");
+       } 
+    public static void main(String[] args) {
+       Task_contructor obj =new Task_contructor();
+   
+    }
+}
